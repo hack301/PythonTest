0ad13f32d9a0844b5da71d1ff41051f00a0f0634
Hack301 <hack301@163.com>
2020-08-06 21:38:23 +0800

    添加相关注释

--- runoob/01.py
+++ runoob/01.py
@@ -19,7 +19,7 @@ print(str[2:])  # 输出第3个到后面的所有字符
 print(str *2)   #   输出2次字符串
 print(str +' Hello')    #   拼接字符串
 print('hello\nrunoob')  #   使用反斜杠  +n换行
-print(r'hello\nrunoob') #   在字符串前面添加一个 r，表示原始字符串，不会发生转义
+print(r'hello\nrunoob') #   在字符串前面添加一个 r，表示原始字符串，不会发生转义是是是
 #   等待用户输入
 #   input("\n\n按下 enter 键后退出。")
 x="a"
