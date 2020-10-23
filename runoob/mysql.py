1558674a218ee34c13a29621a9734e19f30c1109
hack301 <hack301>
2020-10-23 10:34:54 +0800

    修改MySQL字符串

--- runoob/mysql.py
+++ runoob/mysql.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 import pymysql
 
-db = pymysql.connect("192.168.192.118", "ISee", "rsJcAWeSeX85aWGD", "ISee")
+db = pymysql.connect("192.168.192.123", "Test01", "pan19881220.", "Test01")
 cursor = db.cursor()
-cursor.execute("select * from User")
+cursor.execute("select * from is_devicecycleparamdata3 limit 0,10")
 data = cursor.fetchall()
 for i in data:
     print(i)
